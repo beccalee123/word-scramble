96779e3f0e8ca0e27cb6f580ab921b965917c32e
Brent Woodward <Brent.Woodward.1@gmail.com>
2018-10-29 21:18:22 -0700

    user score retrieved from local storage and sorted into high score

--- js/score.js
+++ js/score.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // High score submitted on game.html
 // submission turned into object via constructor
@@ -12,6 +12,9 @@
 // sorts table array by highest score
 // creates and appends table to DOM
 
+
+//ISSUE: if this page is visited before the user plays a game the local storage retrieval will get really updset.
+
 // +++++++++++++++++++++++++++ DATA ++++++++++++++++++++++++++++++
 var allHighScores = [];
 var highScoreTable = document.getElementById('high-score-table');
@@ -22,21 +25,25 @@ function Highscore(userName, score) {
   allHighScores.push(this);
 }
 
-if (!localStorage.highScoreData) {
-new Highscore('Becca', 20);
-new Highscore('Brent', 32);
-new Highscore('Demi', 45);
-new Highscore('Fletcher', 60);
-new Highscore('Jake', 70);
-new Highscore('Bird', 7);
-new Highscore('Noah', 45);
-new Highscore('Tara', 62);
-new Highscore('Zahra', 90);
-new Highscore('Sam', 40);
+// check for local storage
+if (localStorage.highScoreData) {
+  var storedHighScoreData = JSON.parse(localStorage.highScoreData);
+
+  for (var i = 0; i < storedHighScoreData.length; i++) {
+    new Highscore(storedHighScoreData[i].userName, storedHighScoreData[i].score);
+  }
+
 } else {
-
-allHighScores = JSON.parse(localStorage.highScoreData);
-
+  new Highscore('Becca', 20);
+  new Highscore('Brent', 32);
+  new Highscore('Demi', 45);
+  new Highscore('Fletcher', 60);
+  new Highscore('Jake', 70);
+  new Highscore('Bird', 7);
+  new Highscore('Noah', 45);
+  new Highscore('Tara', 62);
+  new Highscore('Zahra', 90);
+  new Highscore('Sam', 40);
 }
 
 // +++++++++++++++++++++++++++ FUNCTION DECLARATIONS +++++++++++++
@@ -49,11 +56,28 @@ Highscore.prototype.renderUserData = function(rank) {
   highScoreTable.appendChild(trEl);
 };
 
+
 function newElement(type, content, parent) {
   var element = document.createElement(type);
   element.textContent = content;
   parent.appendChild(element);
 }
+
+//retrieves username and score from local storage
+function updateNewScore() {
+  if (localStorage.endGameScore) {
+    var endGameScore = JSON.parse(localStorage.endGameScore);
+    new Highscore(endGameScore[0], endGameScore[1]);
+  }
+}
+
+// ---- test for local storage check
+// function testLocalStorage() {
+//   var endGameScore = ['Charles', 65];
+//   localStorage.setItem('endGameScore', JSON.stringify(endGameScore));
+// }
+
+//need to prevent data from redoing itself upon refresh
 
 function makeHeaderRow() {
   var theadEl = document.createElement('thead');
@@ -63,21 +87,25 @@ function makeHeaderRow() {
   highScoreTable.appendChild(theadEl);
 }
 
+//sorts instances of Highscore
 function sortScores() {
   allHighScores.sort(function (a, b) {
     return b.score - a.score;
   });
 }
 
+//renders header, retrieve local storage for new score, sort scores, render table, store table array
 function renderHighScores() {
+  makeHeaderRow();
+  updateNewScore();
   sortScores();
   for (var i = 0; i < allHighScores.length; i++) {
     allHighScores[i].renderUserData(i + 1);
   }
+  localStorage.setItem('highScoreData', JSON.stringify(allHighScores));
 }
 
 // +++++++++++++++++++++++++++ EXECUTABLE ++++++++++++++++++++++++
-makeHeaderRow();
 renderHighScores();
 
 // +++++++++++++++++++++++++++ WIP +++++++++++++++++++++++++++++++
