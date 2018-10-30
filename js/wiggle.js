5fe5e10b1fce4197d45266830b942e7d14292ff0
Fletcher LaRue <laruefletcher@gmail.com>
2018-10-30 14:03:03 -0700

    more testing of animated letters

--- js/wiggle.js
+++ js/wiggle.js
@@ -1,40 +1,29 @@
 'use strict';
 
 document.getElementById('stop').addEventListener('click', stopAnimate);
+document.getElementById('start').addEventListener('click', startAnimate);
+
 var animate;
 
-var word = 'testword';
+var word = 'test';
 var wordArray = [];
 
 for (var i = 0; i < word.length; i++){
     wordArray[i] = word[i];
-    //console.log('wordArray: ', wordArray);
+    // console.log('wordArray: ', wordArray);
 }
 
-for (var i = 0; i < wordArray.length; i++){
-    var liEl = document.createElement('li');
-    liEl.textContent = wordArray[i];
-    var ulEl = document.getElementsByTagName('ul')[0];
-    ulEl.appendChild(liEl);
-}
 
+
+// create canvas
 var canvasEl = document.getElementById('canvas');
 var ctx = canvasEl.getContext("2d");
 ctx.font = "2em monospace";
 
-function update() {
-    clear();
-    var d = Date.now();
-    console.log('d: ', d);
-    ctx.fillStyle = "black";
-    ctx.fillText(`${d}`, 10, 50);
-    //ctx.fill();
-    
-}
-
 // https://codepen.io/tholman/pen/lDLhk?editors=1010
 // call update
-function wiggle() {
+function startAnimate() {
+    // one time function cals
     animate = setInterval(update, 50); // call update every 100ms
 }
 
@@ -44,13 +33,93 @@ function stopAnimate(){
 }
 
 // clear canvas to white background
-function clear() {
+function clearCanvas() {
     ctx.fillStyle = "white";
     ctx.fillRect(0,0,canvasEl.width,canvasEl.height);
 }
 
-wiggle();
+// update function , called constantly
+function update() {
+    
+    clearCanvas();
+    // for each letter
+    for (var i = 0; i < allLetters.length; i++){
+        // draw the letter
+        allLetters[i].draw();
+        //update the letter position
+        // allLetters[i].update();
+    }
+}
 
+// create letters
+// create letter objects
+var allLetters = [];
+for (var i = 0; i < wordArray.length; i++){
+    new Letter(wordArray[i]);
+}
 
+// call this function to render the letters initially
+function renderInitial(){
+    var numLetters = wordArray.length;
+    var letterSpacing = 25;
 
+    var wordTopLeftX = 50;
+    var wordTopLeftY = 20;
+    for (var i = 0; i < allLetters.length; i++){
+        // set initial position
+        // console.log('wordArray[i].xInitial: ', wordArray[i].xInitial);
+        // console.log('wordTopLeftX + letterSpacing * i: ', wordTopLeftX + letterSpacing * i);
+        allLetters[i].xInitial = wordTopLeftX + letterSpacing * i;
+        allLetters[i].yInitial = wordTopLeftY;
+        allLetters[i].asdf();
+    }
+}
+renderInitial();
 
+function Letter(letter) {
+    this.letter = letter;
+    this.xPosition;
+    this.yPosition;
+    this.xSpeed = 2;
+    this.ySpeed;
+    this.xAcceleration;
+    this.yAcceleration;
+
+    this.xInitial = 0;
+    this.yInitial;
+
+    allLetters.push(this);
+}
+
+Letter.prototype.wiggle = function() {
+    // TODO: add function to wiggle
+}
+
+Letter.prototype.move = function(startX, startY, endX, endY) {
+    // TODO: add function to move the letter
+}
+
+Letter.prototype.draw = function() {
+    // TODO: add function to draw the letter
+    // set the letter color
+    ctx.fillStyle = "black";
+
+    // draw the letter at current position
+    ctx.fillText(`${this.letter}`, this.xPosition, this.yPosition);
+}
+
+Letter.prototype.update = function() {
+    // TODO: add function to increment position
+    
+    if (this.xPosition <= 100 ){
+        this.xPosition += this.xSpeed;
+        
+    } else {
+        this.xSpeed = this.xSpeed * -1;
+    }
+}
+
+Letter.prototype.asdf = function() {
+    this.xPosition = xInitial;
+    this.yPosition = yInitial;
+}
