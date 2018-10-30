ba3c20d1c3e07d6a11f3123dc5109f3d2b01ed62
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-10-29 18:16:59 -0700

    Replaces alerts with messages rendered to the DOM

--- js/input-checker.js
+++ js/input-checker.js
@@ -9,40 +9,39 @@ var scrambleSubmission = document.getElementById('scramble-submit');
 var input = document.getElementById('input');
 
 //Function for form clearing?
-function clearInput(){
+function clearInput() {
   input.value = '';
 };
 
 //FORM SUBMISSION
-var handleScrambleSubmission = function(event){
+var handleScrambleSubmission = function (event) {
   console.log(`the user submitted an answer`);
   //prevent page reload on submission. Will need to add this back in, but is currently breaking the keyup function.
   //event.preventDefault();
   //prevent empty fields
-  if (input.value == ''){
-  return alert('Field cannot be empty');
-  //check for correct word
-  } else if (input.value === testWord){ //this will need to be updated for final version to reflect word scramble code setup
+  if (input.value == '') {
+    document.getElementById('alerts').innerHTML = 'Field cannot be empty';
+    //check for correct word
+  } else if (input.value === testWord) { //this will need to be updated for final version to reflect word scramble code setup
+    document.getElementById('alerts').innerHTML = `${input.value} was the correct word - good job!`;
     clearInput();
-  return alert('Good job!!!'); 
-  //replace alert above. 
-  //Add functionality to cue up the next word
-  //Add functionality to add 15 seconds to timer up to max of 5 min
-  //Add functionality to add to score tally based on number of letters in word
-  } else if (input.value === altWord){
+    //Add functionality to cue up the next word
+    //Add functionality to add 15 seconds to timer up to max of 5 min
+    //Add functionality to add to score tally based on number of letters in word
+  } else if (input.value === altWord) {
+    document.getElementById('alerts').innerHTML = `${input.value} is a real word, but we're looking for something with an Ocean theme.`;
     clearInput();
-    return alert(`${input.value} is a real word, but we're looking for something with an Ocean theme.`);
-  } else if (input.value !== testWord){
+  } else if (input.value !== testWord) {
+    document.getElementById('alerts').innerHTML = `Nice try, but ${input.value} is not correct. Try again!`;
     clearInput();
-    return alert(`Nice try, but ${input.value} is not correct. Try again!`);
   }
 }
 
 //ADD EVENT LISTENER
 
 scrambleSubmission.addEventListener('click', handleScrambleSubmission);
-input.addEventListener('keyup', function(e) {
-  if (e.which === 13){
+input.addEventListener('keyup', function (e) {
+  if (e.which === 13) {
     handleScrambleSubmission();
     event.preventDefault();
   }
