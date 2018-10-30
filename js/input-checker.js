1bc3ae5936d277f05a1ad4b1b16f025d0e283eac
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-10-29 18:35:36 -0700

    Adds uppercase input functionality.

--- js/input-checker.js
+++ js/input-checker.js
@@ -46,3 +46,23 @@ input.addEventListener('keyup', function (e) {
     event.preventDefault();
   }
 });
+
+// forceKeyPressUppercase function sourced from  https://www.the-art-of-web.com/html/input-field-uppercase/
+
+function forceKeyPressUppercase(e)
+  {
+    var charInput = e.keyCode;
+    if((charInput >= 97) && (charInput <= 122)) { // lowercase
+      if(!e.ctrlKey && !e.shiftKey && !e.metaKey && !e.altKEY) { // no modifier key
+        var newChar = charInput - 32;
+        var start = e.target.selectionStart;
+        var end = e.target.selectionEnd;
+        e.target.value = e.target.value.substring(0, start) + String.fromCharCode(newChar) + e.target.value.substring(end);
+        e.target.setSelectionRange(start+1, start+1);
+        e.preventDefault();
+      }
+    }
+  }
+
+  document.getElementById('input').addEventListener('keypress', forceKeyPressUppercase, false);
+  document.getElementById('input').addEventListener('keypress', forceKeyPressUppercase, false);
