90ba3cdfe0a33bf34e87b05ad45569fbe1c56c70
Fletcher LaRue <laruefletcher@gmail.com>
2018-10-31 13:34:46 -0700

    adds recolor of nav bar on page load for scores page

--- js/score.js
+++ js/score.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// change nav item color
+document.getElementsByTagName('li')[1].style.backgroundColor = 'lightblue';
+
 // High score submitted on game.html
 // submission turned into object via constructor
 // object stored in local storage
